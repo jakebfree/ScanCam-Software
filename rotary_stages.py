61ce9f65a072bb4b5e09ea19b847b0289fc09868
Jake Freeman <jacob.freeman@colorado.edu>
2013-02-26 15:23:29 -0500

    class derived from zaber_device that supports meaningful real-world units for use with Zaber's T-RS rotary stage. http://www.zaber.com/products/product_group.php?group=T-RS&&tab=Documents%20and%20Drawings&name=Motorized_Rotary_Stages The default degree to microstep mapping is for the T-RS60A model

---
+++ rotary_stages.py
@@ -0,0 +1,46 @@
+from zaber_device import *
+
+
+# Units are all with respect to 1 deg 
+rotary_units = {
+        'deg':                       1,
+        'rad':                       (3.1416/180),
+        }
+
+class rotary_stage(zaber_device):
+    ''' rotary_stage(connection, 
+                 device_number,
+                 id, 
+                 deg_per_step = .000234375 * 64,   
+                 units = 'deg',
+                 run_mode = CONTINUOUS,
+                 action_handler = None,
+                 verbose = False):
+    
+    Modified zaber_device with support for more meaningful linear units.
+
+    deg_per_step is the number of degrees the stage turns with one step
+
+
+    See the documentation for zaber_device for the full usage.
+    '''
+    def __init__(self, 
+                 connection, 
+                 device_number, 
+                 id = None,
+                 deg_per_step = .000234375 * 64,   # datasheet microstep size times microsteps per step
+                 units = 'deg',
+                 run_mode = CONTINUOUS,
+                 action_handler = None,
+                 verbose = False):
+
+        self.move_units = units
+        
+        zaber_device.__init__(self, connection, device_number, id = id,
+                units_per_step = deg_per_step*rotary_units[self.move_units],
+                run_mode = run_mode,
+                action_handler = action_handler,
+                verbose = verbose)
+        
+
+
