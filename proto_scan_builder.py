f8dd3c70c1673d60d561377bed5cb8f4072859b5
Jake Freeman <jacob.freeman@colorado.eu>
2013-04-25 12:25:00 -0700

    build standard, corners, and centers files
    The second two may be useful for location verification and depth calibrations.
    
    Also explicitly specify the video clip durations.

--- proto_scan_builder.py
+++ proto_scan_builder.py
@@ -4,24 +4,44 @@ import scancam
 
 # Heuristically found culture geometry on prototype
 # generate scan from calculated corner
-proto_centers = scancam.six_well_biocell_scan( {'x':69.0, 'y':29.2 },
-                          scan_id = 'proto_centers2',
-                          num_h_scan_points = 1,
-                          num_v_scan_points = 1,
-                          video_format_params = { 'subsampling': 3,
-                                           'cropping': (320, 2240, 0, 1920) },
-                          verbose = True )
-
-proto_corners = scancam.six_well_biocell_just_corners_scan( {'x':69.0, 'y':29.2 },
-                          scan_id = 'proto_corners2',
+proto = scancam.six_well_biocell_scan( {'x':69.0, 'y':29.2 },
+                          scan_id = 'proto',
                           num_h_scan_points = 3,
                           num_v_scan_points = 4,
+                          clip_duration = 15,
                           video_format_params = { 'subsampling': 3,
                                            'cropping': (320, 2240, 0, 1920) },
                           verbose = True )
 
-scanfile = open("proto2.scan", 'wb')
+scanfile = open("proto.scan", 'wb')
+pickle.dump( [proto], scanfile)
+scanfile.close()
 
-pickle.dump( [proto_centers, proto_corners], scanfile)
 
+proto_corners = scancam.six_well_biocell_scan( {'x':69.0, 'y':29.2 },
+                          scan_id = 'proto2',
+                          num_h_scan_points = 3,
+                          num_v_scan_points = 4,
+                          clip_duration = 5,
+                          video_format_params = { 'subsampling': 3,
+                                           'cropping': (320, 2240, 0, 1920) },
+                          verbose = True,
+                          just_corners = True )
+
+scanfile = open("proto_corners.scan", 'wb')
+pickle.dump( [proto_corners], scanfile)
+scanfile.close()
+
+
+proto_centers = scancam.six_well_biocell_scan( {'x':69.0, 'y':29.2 },
+                          scan_id = 'proto2',
+                          num_h_scan_points = 1,
+                          num_v_scan_points = 1,
+                          clip_duration = 5,
+                          video_format_params = { 'subsampling': 3,
+                                           'cropping': (320, 2240, 0, 1920) },
+                          verbose = True )
+
+scanfile = open("proto_centers.scan", 'wb')
+pickle.dump( [proto_centers], scanfile)
 scanfile.close()
